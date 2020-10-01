9acc2615593b8ce57528fc0641c371109a17df7f
Jerzy Wilczek <jerzywilczek@student.agh.edu.pl>
2020-10-01 20:37:28 +0200

    Przepisane z uzywaniem Stream'ow, dodane IllegalArgumentException dla blednych argumentow

--- src/agh/cs/lab1/World.java
+++ src/agh/cs/lab1/World.java
@@ -1,55 +1,36 @@
 package agh.cs.lab1;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static java.lang.System.out;
 
 public class World {
     public static void main(String[] args){
         out.println("Start");
         if(args.length > 0) {
-            run(convertFromStringToDirections(args));
+            if(!Arrays.stream(args).allMatch( a -> a.equals("f") || a.equals("b") || a.equals("r") || a.equals("l") )){
+                throw new IllegalArgumentException("All arguments must come from the set {f, b, r, l}");
+            }
+            List<Directions> list = Arrays.stream(args)
+                    .map(a -> {
+                        if(a.equals("f")) return Directions.FORWARD;
+                        if(a.equals("b")) return Directions.BACKWARD;
+                        if(a.equals("r")) return Directions.RIGHT;
+                        else return Directions.LEFT;
+                    })
+                    .collect(Collectors.toList());
+            run(list);
         }
         out.println("Stop");
     }
 
-    public static Directions[] convertFromStringToDirections(String[] args){
-        ArrayList<Directions> dirsTemp = new ArrayList<>();
-        for (String arg : args) {
-            switch (arg) {
-                case "f":
-                    dirsTemp.add(Directions.FORWARD);
-                    break;
-                case "b":
-                    dirsTemp.add(Directions.BACKWARD);
-                    break;
-                case "r":
-                    dirsTemp.add(Directions.RIGHT);
-                    break;
-                case "l":
-                    dirsTemp.add(Directions.LEFT);
-                    break;
-            }
-        }
-        Directions[] dirs = new Directions[dirsTemp.size()];
-        dirsTemp.toArray(dirs);
-        return dirs;
-    }
-
-    public static void run(Directions[] args){
-            for (Directions arg : args) {
-                switch(arg){
-                    case FORWARD:
-                        out.println("Zwierzak idzie do przodu.");
-                        break;
-                    case BACKWARD:
-                        out.println("Zwierzak idzie do tylu.");
-                        break;
-                    case RIGHT:
-                        out.println("Zwierzak skreca w prawo.");
-                        break;
-                    case LEFT:
-                        out.println("Zwierzak skreca w lewo.");
-                        break;
-                }
-            }
+    public static void run(List<Directions> args){
+            args.forEach(d -> {
+                if(d == Directions.FORWARD) out.println("Zwierzak idzie do przodu");
+                if(d == Directions.BACKWARD) out.println("Zwierzak idzie do tylu");
+                if(d == Directions.RIGHT) out.println("Zwierzak skreca w prawo");
+                if(d == Directions.LEFT) out.println("Zwierzak skreca w lewo");
+            });
     }
 }
