2c351b783a3a37b145fa0ad653b3e5863d615704
Jerzy Wilczek <jerzywilczek@student.agh.edu.pl>
2020-11-12 15:24:35 +0100

    make OptionsParserTest work

--- src/test/agh/cs/lab1/OptionsParserTest.java
+++ src/test/agh/cs/lab1/OptionsParserTest.java
@@ -3,21 +3,22 @@ package agh.cs.lab1;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertThrows;
 
 public class OptionsParserTest {
-    /*@Test
+    @Test
     public void parseTest() {
         String[] s1 = {"forward", "b", "left", "r"};
         MoveDirection[] r1 = {MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.RIGHT};
 
         String[] s2 = {"abc", "f", "def", "backward", "123"};
-        MoveDirection[] r2 = {MoveDirection.FORWARD, MoveDirection.BACKWARD};
 
         String[] s3 = {"forward", "BACKWARD", "\n", "324", "l", "r"};
-        MoveDirection[] r3 = {MoveDirection.FORWARD, MoveDirection.LEFT, MoveDirection.RIGHT};
 
-        assertArrayEquals(r1, OptionsParser.parse(s1));
-        assertArrayEquals(r2, OptionsParser.parse(s2));
-        assertArrayEquals(r3, OptionsParser.parse(s3));
-    }*/
+        OptionsParser parser = new OptionsParser();
+
+        assertArrayEquals(r1, parser.parse(s1));
+        assertThrows(IllegalArgumentException.class, () -> parser.parse(s2));
+        assertThrows(IllegalArgumentException.class, () -> parser.parse(s3));
+    }
 }
