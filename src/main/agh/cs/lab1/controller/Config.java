c66121067510151ccdf7a34297e6a4ed2bc9f205
jerzy <jerzykwilczek@gmail.com>
2020-11-29 17:49:05 +0100

    add Config with mock values for now

---
+++ src/main/agh/cs/lab1/controller/Config.java
@@ -0,0 +1,14 @@
+package agh.cs.lab1.controller;
+
+public class Config {
+    public final int width, height, startEnergy, moveEnergy, plantEnergy;
+    public final double jungleRatio;
+    public Config(){
+        width = 200;
+        height = 100;
+        startEnergy = 10;
+        moveEnergy = 1;
+        plantEnergy = 5;
+        jungleRatio = 0.1;
+    }
+}
